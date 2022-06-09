25d75a7a45db3225beec12d0a5cbfcec892b42b2
Sneha <snehac@xello.world>
2022-06-08 23:34:12 -0400

    Adding in API

--- src/app.js
+++ src/app.js
@@ -21,13 +21,15 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function(day) {
-      forecastHTML = forecastHTML + `
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
         <div class="col-2">
           <div class="weather-forecast-date">${day}</div>
           <img
@@ -40,11 +42,16 @@ function displayForecast() {
             <span class="weather-forecast-temperature-min">12°</span>
           </div>
         </div>`;
+  });
 
-  })
-
-    forecastHTML = forecastHTML + `</div>`
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    let apiKey = "da0c57942e9c74d17c49b03c27d59fcb";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -69,6 +76,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coords);
 }
 
 function search(city) {
